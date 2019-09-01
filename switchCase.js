1deea92b99db9120da7c22bf9c60c0cb685ae2c2
Syaroful Umam <umam.syaroful@gmail.com>
2019-09-01 22:54:11 +0700

    switchcase jitu

---
+++ switchCase.js
@@ -0,0 +1,101 @@
+var hari = 2
+var bulan = "Januari"
+var tahun = 1926
+
+switch (hari) {
+    case 1:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 2:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 3:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 4:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 5:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 6:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 7:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 8:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 9:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 10:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 11:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 12:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 13:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 14:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 15:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 16:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 17:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 18:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 19:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 20:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 21:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 22:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 23:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 24:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 25:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 26:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 27:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 28:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 29:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 30:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+    case 31:
+        console.log(hari + " " + bulan + " " + tahun)
+    break
+default:
+console.log("gak jelas tanggal berapa")
+}
